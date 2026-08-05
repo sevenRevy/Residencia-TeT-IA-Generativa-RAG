ead080bb9f73805c78678929a3a8e5c3ce834b59
sevenRevy <jpfr13@gmail.com>
2026-08-05 15:45:14 -0500

    aula02: processamento de PDFs e extração de metadados

---
+++ test_docling.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from docling.document_converter import DocumentConverter
+
+
+## INPUT
+input_file = Path("Aula02_arquivos/twitter_algoritmo.pdf")
+
+## ENV
+output_folder = Path("Aula02_arquivos_output")
+output_folder.mkdir(parents=True, exist_ok=True)
+os.environ["TORCH_COMPILE_DISABLE"] = "1"
+
+### 
+
+converter = DocumentConverter()
+
+result = converter.convert(input_file)
+
+markdown = result.document.export_to_markdown()
+
+## Output
+
+output_file = output_folder / f"{input_file.stem}.md"
+
+output_file.write_text(
+    markdown,
+    encoding="utf-8"
+)
+
+print(f"Saved to: {output_file}")
